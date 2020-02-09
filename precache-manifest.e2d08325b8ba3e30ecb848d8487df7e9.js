7ad6415b6b51100e9ee70bc04642694d7844e19f
ma0c <ma0@contraslash.com>
2020-02-09 08:45:50 -0500

    Updates

---
+++ precache-manifest.e2d08325b8ba3e30ecb848d8487df7e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ccb9608d5de8d682484190b9cbca370",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efc6369b64c4a1732db5",
+    "url": "/static/js/2.6d108fcf.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.6d108fcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34a932c04a2dc46ff5d7",
+    "url": "/static/js/main.6ca337eb.chunk.js"
+  },
+  {
+    "revision": "c8606fd33eeff9e688ce",
+    "url": "/static/js/runtime-main.5559613e.js"
+  }
+]);
